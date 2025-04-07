3708d7fec7a35a8c5a0ab6d183d9398317cbb7eb
ali-osaid01 <aliusaid55@gmail.com>
2025-04-07 08:21:46 +0500

    refactor: replace getHello with healthCheck in AppService and update AppController; remove old ESLint config and add new one; add docker-compose for PostgreSQL

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  healthCheck(): string {
+    const message = "Health Check "
+    return message;
   }
 }
